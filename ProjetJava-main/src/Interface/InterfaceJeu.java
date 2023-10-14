9808963cf0a021eaceb5406f35978e73809725ff
missShit <shyrelt.etu@gmail.com>
2023-10-14 18:46:05 +0200

    AFFichage du popUp presque terminé +  Code CLEAN

--- ProjetJava-main/src/Interface/InterfaceJeu.java
+++ ProjetJava-main/src/Interface/InterfaceJeu.java
@@ -1,13 +1,9 @@
 package Interface;
 import Representation.* ;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-
 import javax.swing.* ;
-import javax.swing.border.MatteBorder;
 
 
 public class InterfaceJeu {
@@ -21,237 +17,221 @@ public class InterfaceJeu {
 		
 		configFenetre();
 	}
-public static JFrame getFenetre() {
-	return fenetre;
-}
 
-
-public static void setFenetre(JFrame fenetre) {
-	InterfaceJeu.fenetre = fenetre;
-}
-
-
-public static JMenuBar getBarreMenu() {
-	return barreMenu;
-}
-
-
-public static void setBarreMenu(JMenuBar barreMenu) {
-	InterfaceJeu.barreMenu = barreMenu;
-}
-
-
-public static void cleanFenetre() {
-	getFenetre().getContentPane().removeAll();
-	getFenetre().revalidate() ;
-	getFenetre().repaint();
-}
-
-
-public static void configMenu() {
-	
-	JMenu inventaire = new JMenu("Inventaire") ;
-	JMenu histoire = new JMenu ("Histoire") ;
-	JMenu statut = new JMenu ("Statut") ;
-	
-	JMenuItem precedent = new JMenuItem("Voir précédent") ;
-	JMenuItem resumeH = new JMenuItem("Résumé de l'histoire") ;
-   JMenuItem stats = new JMenuItem("Statistiques") ;
-	
-	histoire.add(precedent) ;
-	histoire.add(resumeH) ;
-    statut.add(stats);
-	
-	getBarreMenu().add(histoire) ;
-	getBarreMenu().add(inventaire) ;
-	getBarreMenu().add(statut) ;
-	
-	
-	getFenetre().setJMenuBar(getBarreMenu());
-
-}
-
-public static void configFenetre() {
-
-getFenetre().setSize(1000, 1000); //taille fenetre
-getFenetre().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE) ; //sortir correctement de la fenetre
-getFenetre().setLocationRelativeTo(null); // centrer la fenetre sur l'ecran
-configMenu() ;
-getFenetre().setVisible(true); // rendre la fenetre visible
-
-
-
-}
-
-/*
-public static void popUp(String description) {
-	JLayeredPane popUP = new JLayeredPane() ;
-	JLabel label = new JLabel("", JLabel.CENTER) ;
-	GridBagConstraints constraints = new GridBagConstraints(); // Utilisation d'un gestionnaire de mise en page FlowLayout
-	popUP.add(label) ;
-	
-	char[] texts = description.toCharArray(); // transformation de la chaine descrition en tableau de char
-    Timer timer = new Timer(20, new ActionListener() { // timer apparition d'un char composant le tableau ci-dessus toutes les 20milisec
-    int index = 0 ; // index pour recuperer chaque char par l'intermediaire du tableau
-    
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (index < texts.length) { //Parcours de la chaine Descrition caractère par caractère grace au tableau texts
-            char nextChar = description.charAt(index); // recuperation dans la chaine de caractere du caractere associé a l'index
-            if(nextChar =='/'){ //creation d'une condition de passage a la ligne : utilisation de balise web (retour chariot ne marchant pas) MAIS A REVOIR 
-                label.setText(label.getText() + "<br>");
-                index++;
-            }
-            else{
-                label.setText(label.getText() + nextChar); // label.getText() récupère le texte actuellement affiche dans la frame. On ajoute le caractere suivant qui compose la chaine Descrition. ATTENTION setText() ne prend que des String(!=char)
-                index++; //on passe au caractere suivant de la chaine de description
-            }
-            
-        } else {
-            ((Timer) e.getSource()).stop(); // Arrête le timer après l'affichage de tous les textes
-            
-            
-			CreateCloseButton(constraints, popUP);
-        }
-        
+    public static JFrame getFenetre() {
+	    return fenetre;
     }
-});
 
-
-timer.start();
-	
-}
-
-
-
-
-public static void CreateCloseButton(GridBagConstraints constraints, JLayeredPane pane) {
-	JButton fermer = new JButton("Fermer"); // création button
-    constraints.gridx = 0;
-    constraints.gridy = 1;
-    getFenetre().add(fermer,constraints); // ajout du bouton a la fenetre
-	constraints.gridx = 1;
-    constraints.gridy = 1;
-    getFenetre().revalidate() ;
-	boutonClosePane(fermer, pane);
-	
-}
-
-public static void retirer (Component component) {
-	
-	getFenetre().remove(component);
-}
-
-public static void boutonClosePane(JButton btn, JLayeredPane pane) {
-	
-	btn.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				retirer(pane) ;
-			}
-			});
-}
-
-*/
-
-
-
-public void afficherNodeBase(Node node) {
-	fenetre.getContentPane().setLayout(null);
-	cleanFenetre() ;
-    JPanel p= new JPanel();
-    p.setBounds(60, 110, 600, 100);
-    fenetre.getContentPane().add(p);
-    JLabel label = new JLabel("", JLabel.CENTER);// affichage description Node 
-    p.add(label); //ajout du label a la fenetre
-    p.setBackground(Color.CYAN); 
-    label.setFont(new Font("Times New Roman", Font.PLAIN, 17));
-    getFenetre().revalidate();
-    getFenetre().repaint();
-
-	
-    char[] texts = node.getDescription().toCharArray(); // transformation de la chaine descrition en tableau de char
-    Timer timer = new Timer(20, new ActionListener() { // timer apparition d'un char composant le tableau ci-dessus toutes les 20milisec
-    int index = 0; // index pour recuperer chaque char par l'intermediaire du tableau
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (index < texts.length) { //Parcours de la chaine Descrition caractère par caractère grace au tableau texts
-            char nextChar = node.getDescription().charAt(index); // recuperation dans la chaine de caractere du caractere associé a l'index
-            if(nextChar =='/'){ //creation d'une condition de passage a la ligne : utilisation de balise web (retour chariot ne marchant pas) MAIS A REVOIR 
-                label.setText(label.getText() + "<br>");
-                index++;
-                
-            }
-            else{
-                label.setText(label.getText() + nextChar); // label.getText() récupère le texte actuellement affiche dans la frame. On ajoute le caractere suivant qui compose la chaine Descrition. ATTENTION setText() ne prend que des String(!=char)
-                index++; //on passe au caractere suivant de la chaine de description
-            }
-            
-        } else {
-            ((Timer) e.getSource()).stop(); // Arrête le timer après l'affichage de tous les textes
-
-            if (node.getClass()== ChooseNode.class){ //Si le node en question est un chooseNode: affichage boutons choix
-                ChooseNodeButton(node);
-            }
-            if(node.getClass()== InnerNode.class){ // Si le node est un innerNode affichage boutonSuivant
-                InnerNodeButton(node);
-            }
-            //ChooseNodeButton();
-			//
-        }
-        
+    public static void setFenetre(JFrame fenetre) {
+	    InterfaceJeu.fenetre = fenetre;
     }
-});
 
 
-timer.start(); 
+    public static JMenuBar getBarreMenu() {
+	    return barreMenu;
+    }
 
-}
 
-/*public void ChooseNextButton(ArrayList options){
-     for (int i = 0; i < getOptions().size(); i++) {
-            final int currentIndex = i; // On récupère l'index actuel en final pour pouvoir l'utiliser ensuite dans l'action listener 
-            constraints.gridx++;
-            JButton btn1 = new JButton(getOptions().get(i).getNom()); // création button
-            getW().add(btn1, constraints); // ajout du bouton a la fenetre
-            btn1.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    // Code à exécuter lorsque le bouton est cliqué
-                    getOptions().get(currentIndex).display();
-                }
-            });
-        }
-}*/
+    public static void setBarreMenu(JMenuBar barreMenu) {
+	    InterfaceJeu.barreMenu = barreMenu;
+    }
 
-public void ChooseNodeButton(Node node){
-    ChooseNode chooseNode;
-    chooseNode=(ChooseNode)node;
-    JPanel j= new JPanel();
-     j.setBounds(661, 109, 194, 323);
-     fenetre.getContentPane().add(j);
-	 j.setLayout(new FlowLayout(FlowLayout.CENTER, 30, 30));
-     j.setBackground(Color.RED); 
-    
-    for (int i = 0; i < chooseNode.getOptions().size(); i++) {
-        final int currentIndex = i;
-        JButton btn1 = new JButton(chooseNode.getOptions().get(i).getNom());
-        btn1.setFont(new Font("Times New Roman", Font.PLAIN, 11));
-        btn1.setBackground(new Color(240, 240,240));
-		btn1.setForeground(new Color(128, 64, 0));
-        j.add(btn1);
-        btn1.addActionListener(new ActionListener() {
+
+    public static void cleanFenetre() {
+	    getFenetre().getContentPane().removeAll();
+	    getFenetre().revalidate() ;
+	    getFenetre().repaint();
+    }
+
+    public static void menu(JButton chooseButton){
+     JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setBounds(0, 0, 1000, 1000);
+        JPanel panel = new JPanel();
+        panel.setBounds(50, 50, 300, 200);
+        panel.setBackground(Color.BLUE);
+        panel.setVisible(false);
+        layeredPane.add(panel, JLayeredPane.DRAG_LAYER);
+        getFenetre().add(layeredPane);
+        chooseButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //chooseNode.getOptions().get(currentIndex).display();
-                JPanel z = new JPanel();
+                if (panel.isVisible()) {
+                    panel.setVisible(false);
+                } else {
+                    panel.setVisible(true);
+                }
+            }
+        });
+}
+
+    public static void configMenu() {
+        JMenu inventaire = new JMenu("Inventaire");
+        JMenu histoire = new JMenu("Histoire");
+        JMenu statut = new JMenu("Statut");
+    
+        JMenuItem precedent = new JMenuItem("Voir précédent");
+        JMenuItem resumeH = new JMenuItem("Résumé de l'histoire");
+        JMenuItem stats = new JMenuItem("Statistiques");
+    
+        histoire.add(precedent);
+        histoire.add(resumeH);
+        statut.add(stats);
+    
+        getBarreMenu().add(histoire);
+        getBarreMenu().add(inventaire);
+        getBarreMenu().add(statut);
+        getFenetre().setJMenuBar(getBarreMenu());
+       
+        
+    }
+
+
+    public static void configFenetre() {
+
+        getFenetre().setSize(1000, 1000); //taille fenetre
+        getFenetre().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE) ; //sortir correctement de la fenetre
+        getFenetre().setLocationRelativeTo(null); // centrer la fenetre sur l'ecran
+        configMenu() ;
+        getFenetre().setVisible(true); // rendre la fenetre visible
+
+    
+    }
+
+    public void afficherNodeBase(Node node) {
+       JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setBounds(0, 0, 1000, 1000);
+        //JPanel backgroundPanel = new JPanel(); // Panneau de fond pour couvrir la page entière
+        //backgroundPanel.setBounds(0, 0, 1000,1000);
+        //backgroundPanel.setBackground(Color.DARK_GRAY); // Couleur de fond
+        //layeredPane.add(backgroundPanel, JLayeredPane.DEFAULT_LAYER);
+        
+        
+
+	    fenetre.getContentPane().setLayout(null);
+	    cleanFenetre() ;
+        JPanel panelText= new JPanel();
+        panelText.setBounds(60, 110, 600, 100);
+        layeredPane.add(panelText, JLayeredPane.POPUP_LAYER);
+        getFenetre().add(layeredPane);
+        JLabel label = new JLabel("", JLabel.CENTER);// affichage description Node 
+        panelText.add(label); //ajout du label a la fenetre
+        panelText.setBackground(Color.CYAN); 
+        label.setFont(new Font("Times New Roman", Font.PLAIN, 17));
+        getFenetre().revalidate();
+        getFenetre().repaint();
+        JButton btn = new JButton("TEST POPUP");
+        panelText.add(btn);
+        menu(btn);
+
+
+	
+        char[] texts = node.getDescription().toCharArray(); // transformation de la chaine descrition en tableau de char
+        Timer timer = new Timer(20, new ActionListener() { // timer apparition d'un char composant le tableau ci-dessus toutes les 20milisec
+        int index = 0; // index pour recuperer chaque char par l'intermediaire du tableau
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (index < texts.length) { //Parcours de la chaine Descrition caractère par caractère grace au tableau texts
+                char nextChar = node.getDescription().charAt(index); // recuperation dans la chaine de caractere du caractere associé a l'index
+                if(nextChar =='/'){ //creation d'une condition de passage a la ligne : utilisation de balise web (retour chariot ne marchant pas) MAIS A REVOIR 
+                    label.setText(label.getText() + "<br>");
+                    index++;
+                
+                }
+                else{
+                    label.setText(label.getText() + nextChar); // label.getText() récupère le texte actuellement affiche dans la frame. On ajoute le caractere suivant qui compose la chaine Descrition. ATTENTION setText() ne prend que des String(!=char)
+                    index++; //on passe au caractere suivant de la chaine de description
+                }
+            
+            } else {
+                ((Timer) e.getSource()).stop(); // Arrête le timer après l'affichage de tous les textes
+
+                if (node.getClass()== ChooseNode.class){ //Si le node en question est un chooseNode: affichage boutons choix
+                    ChooseNodeButton(node);
+                }
+                if(node.getClass()== InnerNode.class){ // Si le node est un innerNode affichage boutonSuivant
+                    InnerNodeButton(node);
+                }
+            
+            }
+        
+        }
+    });
+
+
+    timer.start(); 
+
+}
+//Fonction creation de bouton chooseNode
+    public void ChooseNodeButton(Node node){
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setBounds(0, 0, 1000, 1000);
+         getFenetre().add(layeredPane);
+        ChooseNode chooseNode;
+        chooseNode=(ChooseNode)node;
+        JPanel panelChoose= new JPanel(); // Cration panel qui contient les boutons choose node
+        panelChoose.setBounds(661, 109, 194, 323);
+        layeredPane.add(panelChoose, JLayeredPane.POPUP_LAYER);
+	    panelChoose.setLayout(new FlowLayout(FlowLayout.CENTER, 30, 30));
+        panelChoose.setBackground(Color.RED); //couleur aleatoire ahah!
+    
+        for (int i = 0; i < chooseNode.getOptions().size(); i++) {
+            final int currentIndex = i;
+            JButton btn1 = new JButton(chooseNode.getOptions().get(i).getNom());
+            btn1.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+            btn1.setBackground(new Color(240, 240,240));
+		    btn1.setForeground(new Color(128, 64, 0));
+            panelChoose.add(btn1);
+           btn1.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    chooseNode.getOptions().get(currentIndex).display(); // clique du bouton provoque affichage du prochain bode
+                
+                }
+            });
+            
+        getFenetre().revalidate();
+        }
+    }
+
+//Creation de bouton Suivant
+    public void InnerNodeButton(Node node){
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setBounds(0, 0, 1000, 1000);
+        JPanel panelInner = new JPanel();
+         getFenetre().add(layeredPane);
+		panelInner.setBounds(711, 494, 144, 62);
+        
+		layeredPane.add(panelInner, JLayeredPane.POPUP_LAYER);
+        panelInner.setBackground(Color.yellow);
+        if(node.getClass()== InnerNode.class){
+            JButton suivant = new JButton("Suivant"); // création button
+                    suivant.setFont(new Font("Times New Roman", Font.PLAIN, 11));
+                    suivant.setBackground(new Color(240, 240,240));
+		            suivant.setForeground(new Color(128, 64, 0));
+                    panelInner.add(suivant);
+                    getFenetre().revalidate() ;
+				    boutonGoNext(suivant, node);
+        }
+    }   
+
+
+    public void boutonGoNext(JButton btn1, Node node){
+        btn1.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            // Code à exécuter lorsque le bouton est cliqué
+                    	    node.goNext().display(); ;
+                    	
+                        }
+            });
+    }
+
+}
+/*JPanel z = new JPanel();
 		        z.setBounds(56, 212, 552, 344);
 		        fenetre.getContentPane().add(z);
 		        z.setLayout(null);
-		
-                
-		
 		        JLayeredPane layeredPane = new JLayeredPane();
 		        layeredPane.setBounds(89, 34, 320, 239);
 		        layeredPane.setForeground(new Color(0, 255, 64));
@@ -279,67 +259,5 @@ public void ChooseNodeButton(Node node){
                 layeredPane.setVisible(false);
 
                 }
-                });
-            }
-        });
-        getFenetre().revalidate();
-    }
-}
-
-
-public void InnerNodeButton(Node node){
-    JPanel k = new JPanel();
-		k.setBounds(711, 494, 144, 62);
-        
-		fenetre.getContentPane().add(k);
-        k.setBackground(Color.yellow);
-     if(node.getClass()== InnerNode.class){
-       JButton suivant = new JButton("Suivant"); // création button
-                suivant.setFont(new Font("Times New Roman", Font.PLAIN, 11));
-                 suivant.setBackground(new Color(240, 240,240));
-		        suivant.setForeground(new Color(128, 64, 0));
-                k.add(suivant);
-                getFenetre().revalidate() ;
-				boutonGoNext(suivant, node);
-    }
-
-
-    /*else{
-        getFenetre().setLayout(new GridBagLayout());
-        GridBagConstraints constraints = new GridBagConstraints(); // Utilisation d'un gestionnaire de mise en page FlowLayout
-        constraints.gridy = -1;
-        constraints.gridx = 0;
-        for (int i = 0; i < getOptions().size(); i++) {
-            final int currentIndex = i; // On récupère l'index actuel en final pour pouvoir l'utiliser ensuite dans l'action listener 
-            constraints.gridx++;
-            JButton btn1 = new JButton(getOptions().get(i).getNom()); // création button
-            getFenetre().add(btn1, constraints); // ajout du bouton a la fenetre
-            btn1.addActionListener(new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    // Code à exécuter lorsque le bouton est cliqué
-                    getOptions().get(currentIndex).display();
-                }
-            });
-        }
-    }*/
-    /*elseif(node.getClass()==FightNode.class){
-
-    }*/
-}
-
-
-public void boutonGoNext(JButton btn1, Node node){
-    btn1.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        // Code à exécuter lorsque le bouton est cliqué
-                    	node.goNext().display(); ;
-                    	
-                    }
-                });
-}
-
-}
-
+                });*/
 
--- ProjetJava-main/src/Representation/ChooseNode.java
+++ ProjetJava-main/src/Representation/ChooseNode.java
@@ -1,7 +1,5 @@
 package Representation;
-import javax.swing.* ; // importation des librairies necessaires
 import java.util.ArrayList;
-import java.awt.*;
 
 public class ChooseNode extends Node {
 	
@@ -11,7 +9,6 @@ public class ChooseNode extends Node {
 	public ChooseNode(String nom, String description, ArrayList <Node> options){
 		super(nom, description) ; // appel au constructeur de la classe mère
 		this.options = options ; 
-		
 	}
 	
 	
@@ -36,33 +33,7 @@ public class ChooseNode extends Node {
 		super.display();
 
     }
-    /* 
 
-
-                    for (int i = 0; i < getOptions().size(); i++) {
-                        final int currentIndex = i; // On récupère l'index actuel en final pour pouvoir l'utiliser ensuite dans l'action listener 
-                        constraints.gridx++;
-                        JButton btn1 = new JButton(getOptions().get(i).getNom()); // création button
-                        getW().add(btn1, constraints); // ajout du bouton a la fenetre
-                        btn1.addActionListener(new ActionListener() {
-                            @Override
-                            public void actionPerformed(ActionEvent e) {
-                                // Code à exécuter lorsque le bouton est cliqué
-                                getOptions().get(currentIndex).display();
-                            }
-                        });
-                    }
-
-                    getW().revalidate() ;
-                    
-                }
-            }
-        });
-        timer.start();
-		
-	}
-	
-	*/
 	 public static void main(String[] args) {
  		InnerNode test = new InnerNode("Node test", "<html> vous êtes un jeune prince/ BLABLABLA Vous avez assassiné le roi etc... ", true) ; // balise html a revoir            		
      	TerminalNode gameOver = new TerminalNode("Game Over" , "<html> Tu viens de mourir grosse merde, veux tu reprendre au check point ou rage quit ?") ;
