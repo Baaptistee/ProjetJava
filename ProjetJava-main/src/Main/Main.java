8e44ed0948591067529035ce049e9445a403fc11
Shyrel <146637464+missShit@users.noreply.github.com>
2023-12-14 12:58:02 +0100

    instance Chevalier

--- ProjetJava-main/src/Main/Main.java
+++ ProjetJava-main/src/Main/Main.java
@@ -22,13 +22,20 @@ public class Main {
 		// Tutoriel : 
 		String joueur = Game.getGame().getNomJoueur() ;
 
+		
 		InnerNode n1 = new TextNode("??? : "+joueur+"... ! / Réveille-toi !!");
 		InnerNode n2 = new ChooseNode("Continuer à dormir", "??? : Aller dêpeche toi on va être en retard !") ;
 		InnerNode n3 = new ChooseNode("Continuer à dormir","??? : "+ joueur.toUpperCase()+" !!!!/ RÉVEILLE TOI !!");
 		InnerNode n4 = new TextNode("Se réveiller", "??? : Ah enfin tu te décides ... Tu sais pourtant très bien qu'aujourd'hui est un jour important./");
 		InnerNode n5 = new TextNode("Leo : Heu t'es sur que ça va ?/Allô la lune ici Leo t'as oublié que la remise des diplômes c'était aujourd'hui ou quoi ?/"+joueur+" : La remise des diplômes ?/Leo : Non mais je rêve ! Tu sais le diplôme de magie pour lequel on sue sang et eau depuis qu'on est gosse ?/C'est enfin le grand jour !!//<em>Les deux garçons se dépêchent d'enfiler leurs tenues et de se diriger vers le hall de la cérémonie.</em>");
 		InnerNode n6 = new TextNode("Leo : Woaaah c'est beaucoup plus impressionant en vrai !/ Jeune fille : Ah enfin vous êtes là vous, je pensais que vous n'arriveriez jamais !/Leo : Désolé Anna, C'est "+joueur+" il se l'est joué Belle au bois Dormant ce matin./"+joueur+" : La ferme Leo, j'ai juste eu du mal à dormir hier soir à cause de tes ronflements. /Leo : Dit le mec qui faisait trembler la chambre ce matin .../<em> Les interrompt</em>/Anna : C'est pas bientôt fini vous deux ? Taisez vous ça va bientôt commencer.");
-		InnerNode n7 = new TextNode("")
+		InnerNode n7 = new TextNode("Le crépuscule enveloppait le château de mystère,/ tandis que "+joueur+", accompagné de son fidèle chevalier, du sage mage et de l'attentionné soigneur,/ décidait de fuir les murs imposants qui avaient été témoins de la tragédie royale./ Les lourdes portes du château s'ouvrirent silencieusement, et l'équipe s'éclipsa dans l'obscurité./ Guidés par l'ombre bienveillante de la nuit,/ "+joueur+" et ses compagnons chevauchèrent à travers les bois embrumés,/ laissant derrière eux les murmures inquiets du château endormi./ ");
+		InnerNode n8= new ChooseNode("history","La lueur pâle de la lune éclairait leur chemin /alors qu'ils traversaient la forêt,/ cherchant refuge dans l'incertitude du destin qui les attendait./Ils ne savaient pas où leurs pas les conduiraient, /mais le besoin pressant de s'éloigner du château empreint de trahison les guidait./ Ils devaient prendre uen decision:");
+		InnerNode n9= new TextNode("Chez Mage", "");
+		InnerNode n10= new TextNode("Chez Chevalier", "Guidés par l'ombre bienveillante de la nuit,/ "+joueur+" et ses compagnons chevauchèrent à travers les bois embrumés,/ laissant derrière eux les murmures inquiets du château endormi./le domaine du père du chevalier émergea des ombres./ Une imposante silhouette de château se dressait contre le ciel nocturne,/ éclairée par la douce lueur des lanternes./ La cour intérieure, encadrée par des murailles de pierre solides,/ accueillit les fugitifs avec l'assurance de la loyauté./ Le portail grinça légèrement lorsqu'ils entrèrent,/ mais le silence qui enveloppait le domaine indiquait que leur arrivée n'avait pas été remarquée./ "+joueur+", le chevalier, le mage et le soigneur se dirigèrent vers la résidence principale, où le père du chevalier les attendait,/ ignorant encore les événements qui avaient secoué le royaume. ");
+		InnerNode n11= new TextNode("Chez Soigneur", "");
+		InnerNode n12 = new TextNode("Dans le hall majestueux du domaine du chevalier,/ le père écouta avec une attention grave le récit du prince et de son équipe./ Les yeux du vieil homme reflétaient l'inquiétude mêlée d'une détermination à défendre l'honneur de son fils et la quête de justice du prince./Cependant, à peine avaient-ils commencé à élaborer leurs plans que les lourds bruits de sabots résonnèrent à l'extérieur./ Les échos de la cavalerie du roi, armée jusqu'aux dents, annonçaient l'arrivée imminente de la garde royale.");
+		
 		n1.addOption(n2);
 		n2.addOption(n4);
 		n2.addOption(n3);
@@ -36,6 +43,12 @@ public class Main {
 		n3.addOption(n2);
 		n4.addOption(n5);
 		n5.addOption(n6);
+		n6.addOption(n7);
+		n7.addOption(n8);
+		n8.addOption(n9);
+		n8.addOption(n10);
+		n8.addOption(n11);
+		n10.addOption(n12);
 
 		
 
